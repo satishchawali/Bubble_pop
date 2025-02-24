8642688b9beb3057e647dc57118e5e59908b48f3
sejalkhandare14021 <45506838+sejalkhandare14021@users.noreply.github.com>
2025-02-24 16:27:15 +0530

    Anish add

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
         System.out.println("First commit");
+        System.out.println("Anish hi");
     }
 }
