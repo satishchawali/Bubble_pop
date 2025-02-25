b3cf67b99f7c5c7364123662540b44e12d782b65
anishuno <anishkhandare1206@gmail.com>
2025-02-26 00:40:26 +0530

    User.java

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-<<<<<<< HEAD
-
-=======
         System.out.println("Test11");
->>>>>>> 3fabfb46b82e775e1e8c0314cb7fd1a8d6a35b3d
     }
 }
