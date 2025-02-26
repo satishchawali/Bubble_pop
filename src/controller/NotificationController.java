fa5cc22368a2a80f71f42f68020bcb24654b778d
Satish Chawali <satishchawali40@gmail.com>
2025-02-26 21:37:56 +0530

    Satish done

--- src/controller/NotificationController.java
+++ src/controller/NotificationController.java
@@ -1,4 +1,21 @@
 package controller;
 
+import database.NotificationDAO;
+import model.Notification;
+import java.sql.Connection;
+
 public class NotificationController {
+    private NotificationDAO notificationDAO;
+
+    public NotificationController(Connection conn) {
+        this.notificationDAO = new NotificationDAO(conn);
+    }
+
+    public void sendNotification(String message, int userId) {
+        try {
+            notificationDAO.saveNotification(new Notification(0, message, userId));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
--- src/controller/PostController.java
+++ src/controller/PostController.java
@@ -1,4 +1,22 @@
+
 package controller;
 
+import database.PostDAO;
+import model.Post;
+import java.sql.Connection;
+
 public class PostController {
+    private PostDAO postDAO;
+
+    public PostController(Connection conn) {
+        this.postDAO = new PostDAO(conn);
+    }
+
+    public void createPost(String content, int userId) {
+        try {
+            postDAO.addPost(new Post(0, content, userId));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
